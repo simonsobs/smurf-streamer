ca35f2e3403571edb144e7296d2452d666f6515a
jlashner <jlashner@gmail.com>
2020-02-10 17:34:14 -0800

    Update pysmurf and stream scripts to use pysmurf common parser

--- scripts/stream.py
+++ scripts/stream.py
@@ -1,43 +1,53 @@
 from spt3g import core
 
-
 import pyrogue.gui
 import argparse
 import sosmurf
 import sys
 import threading
-import p
+import pysmurf.core.server_scripts.Common as pysmurf_common
 
 from pysmurf.core.roots.CmbEth import CmbEth
 
 def main():
-    parser = sosmurf.util.make_smurf_parser()
+    parser = pysmurf_common.make_parser()
 
-    parser.add_argument('--dev', action='store_true',
-        help = "If set, use DevBoardEth context manager instead of CmbEth"
-    )
     parser.add_argument('--stream-port', type=int, default=4536)
+    parser.add_argument('--stream-id', type=str)
 
     args = parser.parse_args()
-    pcie_kwargs, root_kwargs = sosmurf.util.process_args(args)
+    pysmurf_common.process_args(args)
+    from pysmurf.core.roots.DevBoardEth import DevBoardEth
+
+    if args.ip_addr is None:
+        raise argparse.ArgumentError("Must specify --addr for ethernet based devices")
 
     builder = sosmurf.SmurfBuilder()
     transmitter = sosmurf.SmurfTransmitter(
         builder, name="SOSmurfTransmitter", debug_meta=False, debug_data=False
     )
-    root_kwargs['txDevice'] = transmitter
 
     pipe = core.G3Pipeline()
     pipe.Add(builder)
+    pipe.Add(sosmurf.SessionManager.SessionManager, stream_id = args.stream_id)
     pipe.Add(core.Dump)
     pipe.Add(core.G3NetworkSender, hostname='*', port=args.stream_port,
                                    max_queue_size=1000)
 
+    vgs = {
+        'root.RogueVersion'     : {'groups' : ['publish','stream'], 'pollInterval': None},
+        'root.RogueDirectory'   : {'groups' : ['publish','stream'], 'pollInterval': None},
+        'root.SmurfApplication' : {'groups' : ['publish','stream'], 'pollInterval': None},
+        'root.SmurfProcessor'   : {'groups' : ['publish','stream'], 'pollInterval': None},
+    }
+
+    pcie_kwargs = sosmurf.util.get_kwargs(args, 'pcie', comm_type='eth-rssi-interleaved')
+    root_kwargs = sosmurf.util.get_kwargs(args,'dev_board_eth', txDevice = transmitter, VariableGroups=vgs)
+    
     with pysmurf.core.devices.PcieCard(**pcie_kwargs):
-        with CmbEth(**root_kwargs) as root:
+        with DevBoardEth(**root_kwargs) as root:
             print("got pysmurf root", flush=True)
-            print("HERE!!", flush=True)
-            if args.gui:
+            if args.use_gui:
                 print("Starting GUI...")
                 app_top = pyrogue.gui.application(sys.argv)
                 gui_top = pyrogue.gui.GuiTop(incGroups=None,excGroups=None)
--- scripts/stream_devboard.py
+++ scripts/stream_devboard.py
@@ -7,22 +7,26 @@ import sosmurf
 import sys
 import threading
 import pysmurf.core.devices
+import pysmurf.core.server_scripts.Common as pysmurf_common
 
 
 def main():
-    parser = sosmurf.util.make_smurf_parser()
+    parser = pysmurf_common.make_parser()
 
     parser.add_argument('--stream-port', type=int, default=4536)
     parser.add_argument('--stream-id', type=str)
 
     args = parser.parse_args()
-    pcie_kwargs, root_kwargs = sosmurf.util.process_args(args)
+    pysmurf_common.process_args(args)
+    from pysmurf.core.roots.DevBoardEth import DevBoardEth
+
+    if args.ip_addr is None:
+        raise argparse.ArgumentError("Must specify --addr for ethernet based devices")
 
     builder = sosmurf.SmurfBuilder()
     transmitter = sosmurf.SmurfTransmitter(
         builder, name="SOSmurfTransmitter", debug_meta=False, debug_data=False
     )
-    root_kwargs['txDevice'] = transmitter
 
     pipe = core.G3Pipeline()
     pipe.Add(builder)
@@ -31,12 +35,20 @@ def main():
     pipe.Add(core.G3NetworkSender, hostname='*', port=args.stream_port,
                                    max_queue_size=1000)
 
-    from pysmurf.core.roots.DevBoardEth import DevBoardEth
+    vgs = {
+        'root.RogueVersion'     : {'groups' : ['publish','stream'], 'pollInterval': None},
+        'root.RogueDirectory'   : {'groups' : ['publish','stream'], 'pollInterval': None},
+        'root.SmurfApplication' : {'groups' : ['publish','stream'], 'pollInterval': None},
+        'root.SmurfProcessor'   : {'groups' : ['publish','stream'], 'pollInterval': None},
+    }
 
+    pcie_kwargs = sosmurf.util.get_kwargs(args, 'pcie', comm_type='eth-rssi-interleaved')
+    root_kwargs = sosmurf.util.get_kwargs(args,'dev_board_eth', txDevice = transmitter, VariableGroups=vgs)
+    
     with pysmurf.core.devices.PcieCard(**pcie_kwargs):
         with DevBoardEth(**root_kwargs) as root:
             print("got pysmurf root", flush=True)
-            if args.gui:
+            if args.use_gui:
                 print("Starting GUI...")
                 app_top = pyrogue.gui.application(sys.argv)
                 gui_top = pyrogue.gui.GuiTop(incGroups=None,excGroups=None)
