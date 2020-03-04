b00f597820ddca5449767154bca02903badb62ee
jlashner <jlashner@gmail.com>
2020-03-03 21:54:08 -0800

    Got emulator working

--- scripts/emulate.py
+++ scripts/emulate.py
@@ -1,58 +1,64 @@
 from spt3g import core
 
-import pysmurf.core.devices
 import pyrogue.gui
+import pysmurf.core.devices
+import pysmurf.core.server_scripts.Common as pysmurf_common
+
 import argparse
+import shlex
 import sosmurf
 import sys
-import threading
 
 
 def main():
-    parser = sosmurf.util.make_smurf_parser()
+
+    parser = pysmurf_common.make_parser()
+
+    # modified_args = sosmurf.util.setup_server()
+
     parser.add_argument('--stream-port', type=int, default=4536)
+    parser.add_argument('--stream-id', type=str)
 
     args = parser.parse_args()
+    pysmurf_common.process_args(args)
 
-    # Setup G3 Pipeline
+
     builder = sosmurf.SmurfBuilder()
     transmitter = sosmurf.SmurfTransmitter(
-        builder, name="SOSmurfTransmitter", debug_meta=False, debug_data=False
+        builder, name="SOSmurfTransmitter", debug_meta=True, debug_data=False
     )
 
     pipe = core.G3Pipeline()
     pipe.Add(builder)
-    pipe.Add(core.Dump)
+    pipe.Add(sosmurf.SessionManager.SessionManager, stream_id=args.stream_id)
+    pipe.Add(sosmurf.util.stream_dumper)
     pipe.Add(core.G3NetworkSender, hostname='*', port=args.stream_port,
                                    max_queue_size=1000)
 
-    emulation_kwargs = {
-        'config_file':     args.config_file,
-        'epics_prefix':    args.epics,
-        'polling_en':      (not args.nopoll),
-        'pv_dump_file':    args.dump_pvs,
-        'disable_bay0':    args.disable_bay0,
-        'disable_bay1':    args.disable_bay1,
-        'txDevice':        transmitter,
+    vgs = {
+        'root.FpgaTopLevel.AppTop.AppCore.enableStreaming': 
+            {'groups' : ['publish','stream'], 'pollInterval': 0}
     }
 
-    with EmulationRoot(**emulation_kwargs) as root:
+
+    # Import the root device after the python path is updated
+    from pysmurf.core.roots.EmulationRoot import EmulationRoot
+
+    with EmulationRoot ( config_file    = args.config_file,
+                         epics_prefix   = args.epics_prefix,
+                         polling_en     = args.polling_en,
+                         pv_dump_file   = args.pv_dump_file,
+                         disable_bay0   = args.disable_bay0,
+                         disable_bay1   = args.disable_bay1,
+                         txDevice       = transmitter,
+                         VariableGroups = vgs) as root:
+        print("Loaded Emulation root", flush=True)
+
+        # Add dummy TES bias values ([-8:7]), for testing purposes.
         for i in range(16):
             root._smurf_processor.setTesBias(index=i, val=(i-8))
 
-        if args.gui:
-            print("Starting GUI...")
-            app_top = pyrogue.gui.application(sys.argv)
-            gui_top = pyrogue.gui.GuiTop(incGroups=None,excGroups=None)
-            gui_top.setWindowTitle(args.windows_title)
-            gui_top.addTree(root)
-            gui_top.resize(800,1000)
-            app_top.exec_()
-
-        else:
-            print("Starting G3Pipeline", flush=True)
-            pipe.Run(profile=True)
-            print("Closed G3 pipeline")
-
+        pipe.Run(profile=True)
+        
 if __name__ == '__main__':
     main()
