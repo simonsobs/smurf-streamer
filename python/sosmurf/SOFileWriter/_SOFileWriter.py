e90ec0e48f9baf91953babc19ce4cae1f0a70dcd
jlashner <jlashner@gmail.com>
2021-04-25 10:35:34 -0400

    Adds SOFileWriter to stream path

---
+++ python/sosmurf/SOFileWriter/_SOFileWriter.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import pyrogue
+from spt3g import core
+import os
+import time
+from sosmurf.SessionManager import FlowControl
+
+class G3Rotator:
+    def __init__(self, data_path, file_dur):
+        self.data_path = data_path
+        self._writer = None
+        self.seq = 0
+        self.cur_session_id = 0
+        self.file_start_time = 0
+        self.file_dur = file_dur
+        self.cur_path = ''
+
+    def get_cur_path(self):
+        return self.cur_path
+
+    def get_session_id(self):
+        return self.cur_session_id
+
+    def close_writer(self):
+        if self._writer is not None:
+            self.cur_path = ''
+            self.file_start_time = 0
+            self.cur_session_id = 0
+            self._writer(core.G3Frame(core.G3FrameType.EndProcessing))
+        self._writer = None
+
+    def new_writer(self, frame, seq):
+        session_id = frame['session_id']
+        stream_id = frame['sostream_id']
+        subdir = os.path.join(self.data_path, stream_id, str(session_id)[:5])
+        fname = f"{session_id}_{seq:03}.g3"
+        fpath = os.path.join(subdir, fname)
+
+        if not os.path.exists(subdir):
+            os.makedirs(subdir)
+
+        self._writer = core.G3Writer(fpath)
+        self.cur_path = fpath
+        self.file_start_time = time.time()
+        self.cur_session_id = session_id
+
+        return self._writer
+
+    def new_file_condition(self, frame):
+        return time.time() > self.file_start_time + self.file_dur
+
+    def get_writer(self, frame):
+        """
+        Gets appropriate G3 writer for a given frame based on the following
+        rules. Will automatically rotate files if `new_file_condition` has
+        been met, and will close out files if an FlowControl.End frame is
+        received.
+        """
+        if 'sostream_flowcontrol' in frame:
+            if frame['sostream_flowcontrol'] == FlowControl.END.value:
+                self.close_writer()
+            return None
+
+        if 'session_id' not in frame:
+            return None
+        sess_id = frame['session_id']
+
+        if self.cur_session_id != sess_id:
+            self.close_writer()
+            self.seq = 0
+            return self.new_writer(frame, self.seq)
+
+        elif self.new_file_condition(frame):
+            self.close_writer()
+            self.seq += 1
+            return self.new_writer(frame, self.seq)
+
+    def __call__(self, frame):
+        writer = self.get_writer(frame)
+        if writer is not None:
+            writer(frame)
+        return [frame]
+
+class SOFileWriter(pyrogue.Device):
+    def __init__(self, name, data_path, file_dur=30*60):
+        pyrogue.Device.__init__(self, name=name, description="G3 File Rotator")
+        self.rotator = G3Rotator(data_path, file_dur)
+        self.add(pyrogue.LocalVariable(
+            name="filepath",
+            description="Path to the current G3 file",
+            mode='RO',
+            value='',
+            localGet=self.rotator.get_cur_path
+        ))
+
+        self.add(pyrogue.LocalVariable(
+            name="session_id",
+            description="Current stream session-id",
+            mode='RO',
+            value=0,
+            localGet=self.rotator.get_session_id
+        ))
--- python/sosmurf/__init__.py
+++ python/sosmurf/__init__.py
@@ -3,3 +3,4 @@ import sosmurf.SessionManager
 
 from sosmurf.SmurfTransmitter import SmurfTransmitter
 from sosmurf.StreamBase import StreamBase
+from sosmurf.SOFileWriter import SOFileWriter
