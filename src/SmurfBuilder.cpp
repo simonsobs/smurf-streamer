767a3ce6aded60435fcb298ae04081825d810593
jlashner <jlashner@gmail.com>
2023-01-31 14:31:09 -0500

    Adds logic for timing_paradigm var

--- src/SmurfBuilder.cpp
+++ src/SmurfBuilder.cpp
@@ -82,7 +82,6 @@ G3FramePtr SmurfBuilder::FrameFromSamples(
 
     alloc_start = std::chrono::system_clock::now();
 
-    TimestampType timing_type = (*start)->GetTimingParadigm();
 
     // Initialize detector timestreams
     int32_t* data_buffer= (int32_t*) calloc(nchans * nsamps, sizeof(int32_t));
@@ -119,6 +118,9 @@ G3FramePtr SmurfBuilder::FrameFromSamples(
 
     copy_start = std::chrono::system_clock::now();
 
+
+    TimestampType timing_type = Timing_LowPrecision;
+
     // Read data in to G3 Objects
     int sample = 0;
     for (auto it = start; it != stop; it++, sample++){
@@ -140,6 +142,10 @@ G3FramePtr SmurfBuilder::FrameFromSamples(
 
         for (int i = 0; i < nchans; i++){
             data_buffer[sample + i * nsamps] = (*it)->sp->getData(i);
+        }
+
+        if (hdr->getCounter2() != 0){
+            timing_type = Timing_HighPrecision;
         }
     }
 
